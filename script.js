17b9d4996d2b2f4b1232770e43d7334dfbd78dea
kaushalsatam <kaushalabd17@gmail.com>
2024-03-01 22:59:37 +0530

    Added more features like showing current day date and can add tasks

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
+const date = new Date();
+
+let week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+let weekDay = week[date.getDay()];
+let day = date.getDate();
+let month = date.getMonth() + 1;
+let year = date.getFullYear();
+
+let currentDate = `${day}-${month}-${year}`;
+
+$("#day").text(weekDay + ",");
+$("#date").text(currentDate);
+
+let noOfTasks = 1;
 
 $("button").on("click", function(){
-    $("form").append(task)
-})
+    $("span.start").remove();
+    let task = prompt("Task name:");
+    let taskAdd = '<div class="bg-sky-300 p-5 rounded-xl mb-4"><label for="task'+ noOfTasks + '" class="h-12 w-auto"><input type="checkbox" name="task" id="task'+ noOfTasks + '" class=" task-checkbox size-5 m-3" />'+ task + '</label></div>'
+    $("form").append(taskAdd);
+    noOfTasks++;
+});
+
